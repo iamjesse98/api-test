3764999663792c41b06163645e5fb62b15c52324
iamjesse98 <yadagallajaswanthkumar98@gmail.com>
2020-05-05 20:22:32 +0530

    second

--- index.js
+++ index.js
@@ -11,18 +11,25 @@ app.use(bodyparser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.status(200).json({ message: "hello" }))
 
+app.post("/test", (req, res) => {
+  console.log(req.body)
+  res.status(200).json(req.body)
+})
+
 app.post("/name", (req, res) => {
   const { name } = req.body;
 
-  console.log(req.bdy)
+  console.log(req.body)
 
   if (name) {
     res.status(200).json({
-      message: "Order created successfully"
+      message: "Order created successfully",
+      ...req.body
     });
   } else {
     res.status(401).json({
-      message: "Invalid Order creation"
+      message: "Invalid Order creation",
+      ...req.body
     });
   }
 });
